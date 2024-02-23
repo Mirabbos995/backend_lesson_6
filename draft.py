694f1c09728c073ae51cf3d7f430a67ff761175c
Mirabbos995 <jahonaliyevmirabbos621@gmail.com>
2024-02-23 21:01:03 +0300

    pro

---
+++ draft.py
@@ -0,0 +1,130 @@
+# x = ('key1', 'key2', 'key3')
+# y = 0
+#
+# thisdict = dict.fromkeys(x, y)
+#
+# print(thisdict)
+
+
+
+
+
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+#
+# x = car.setdefault("brand", "Bronco")
+#
+# print(x)
+
+
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+#
+# car.clear()
+#
+# print(car)
+
+
+
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+#
+# x = car.copy()
+#
+# print(x)
+
+
+
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+#
+# x = car.get("year")
+#
+# print(x)
+
+
+
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+#
+# x = car.items()
+#
+# print(x)
+
+
+
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+#
+# x = car.keys()
+#
+# print(x)
+
+
+
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+#
+# car.pop("model")
+#
+# print(car)
+
+
+
+
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964                          # removes the last item
+# }
+#
+# car.popitem()
+#
+# print(car)
+
+
+
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964                            # adds item to the end
+# }
+#
+# car.update({"color": "White"})
+#
+# print(car)
+
+
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+#
+# x = car.values()
+#
+# print(x)
+
+
+
---
+++ main.py
@@ -0,0 +1,141 @@
+# def maximum_score(lst):                                    # 1 - misol
+#     a = 0
+#     for x in lst:
+#         a += x.get("score")
+#     return a
+#
+#
+# print(maximum_score([
+#   { "tile": "N", "score": 1 },
+#   { "tile": "K", "score": 5 },
+#   { "tile": "Z", "score": 10 },
+#   { "tile": "X", "score": 8 },
+#   { "tile": "D", "score": 2 },
+#   { "tile": "A", "score": 1 },
+#   { "tile": "E", "score": 1 }
+# ]))
+#
+# print(maximum_score([
+#   { "tile": "N", "score": 1 },
+#   { "tile": "K", "score": 5 },
+#   { "tile": "Z", "score": 10 },
+#   { "tile": "X", "score": 8 },
+#   { "tile": "D", "score": 2 },
+#   { "tile": "A", "score": 1 },
+#   { "tile": "E", "score": 1 }
+# ]))
+#
+# print(maximum_score([
+#   { "tile": "B", "score": 2 },
+#   { "tile": "V", "score": 4 },
+#   { "tile": "F", "score": 4 },
+#   { "tile": "U", "score": 1 },
+#   { "tile": "D", "score": 2 },
+#   { "tile": "O", "score": 1 },
+#   { "tile": "U", "score": 1 }
+# ]))
+import xml.dom
+
+# Make a Circle with OOP      #2 - misol
+a = int(input("❗son❗:"))
+resault = 0
+calc_diff = {"skate": 200, "painting": 200, "shoes": 1 }
+for x in calc_diff.values():
+    resault = resault + x
+    y = resault - a
+print(y)
+
+
+
+
+
+
+# def mapping(lst):              #3 - misol
+#     letter = {}
+#     for x in lst:
+#         letter[x] = x.upper()
+#     return letter
+
+# print(mapping(["p", "s"]))
+# print(mapping(["a", "b", "c"]))
+
+
+
+
+
+                                    # 4 - misool
+# count_all = input("Enter: ")
+# a = {}
+
+# d = sum(x.isalpha() for x in count_all)
+# b = sum(x.isdigit() for x in count_all)
+
+
+# print(f"Letter: {d}, Digits: {b}")
+
+
+
+
+# Burglary Series (10): Calculate Difference     # 5 - misol
+
+
+
+
+
+
+# Pythagorean theorem   # 6 - misol
+# not done
+
+
+
+# Planetary Weight Converter   # 7 - misol
+# a = input("Enter the planet: ")
+# b = input("Enter the planet: ")
+# d = 1
+#
+# gravitational_forces = {
+#     "Mercury": 3.7,
+#     "Venus": 8.87,
+#     "Earth": 9.81,
+#     "Mars": 3.711,
+#     "Jupiter": 24.79,
+#     "Saturn": 10.44,
+#     "Uranus": 8.69,
+#     "Neptune": 11.15
+# }
+#
+# if a in gravitational_forces:
+#     c = d / gravitational_forces[a]
+#     k = c * gravitational_forces[b]
+#     print(k)
+
+
+# Older Than Me   # 8 - misol
+
+
+
+
+
+
+# Convert Key, Values in a Dictionary to List   # 9 - misol
+
+# def dict_to_list(lst):
+#     list = []
+#     for x in lst.items():
+#         list.append(x)
+#     list.sort()
+#     return list
+#
+# print(dict_to_list({
+#   "D": 1,
+#   "B": 2,
+#   "C": 3
+# }))
+
+
+# Frequency Distribution    # 10 - misol
+
+
+
+
+
